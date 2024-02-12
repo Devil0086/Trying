cfd3904b6dfde3daddca142d89531cd6d14c844c
Devil0086 <152967696+Devil0086@users.noreply.github.com>
2024-02-13 01:46:46 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,7 +4,7 @@
 //=======[Webhook]========https://api.telegram.org/bot1917420727:AAF9RuwMqWfLh3wQTed_jrkJ-lsTLuGZPe4/setwebhook?url=<url>
 
 //================[Telegram BOT API TOKEN ]================//
-$botToken = "1917420727:AAF9RuwMqWfLh3wQTed_jrkJ-lsTLuGZPe4"; #<------------------- PUT YOUR TOKEN HERE------------->#
+$botToken = "6655307320:AAHqhFS4A7eO1tgpQHYOh33gZaPs5sxQUSE"; #<------------------- PUT YOUR TOKEN HERE------------->#
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
